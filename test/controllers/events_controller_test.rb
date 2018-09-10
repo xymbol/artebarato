5e0bd504c2f2e92043b4148b4206aa772d58957c
Adrián Mugnolo <adrian@mugnolo.com>
2018-09-10 15:20:08 -0300

    Test authentication

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -4,12 +4,30 @@ require 'test_helper'
 
 class EventsControllerTest < ActionDispatch::IntegrationTest
   test 'index' do
-    get root_url
+    get root_url, auth_headers
     assert_response :success
   end
 
+  test 'index without auth' do
+    get root_url
+    assert_response :unauthorized
+  end
+
   test 'index with category' do
-    get category_events_url(categories(:musica))
+    get category_events_url(categories(:musica)), auth_headers
     assert_response :success
+  end
+
+  def auth_credentials
+    ActionController::HttpAuthentication::Basic.encode_credentials \
+      'admin', 'secret'
+  end
+
+  def auth_headers
+    {
+      headers: {
+        Authorization: auth_credentials
+      }
+    }
   end
 end
